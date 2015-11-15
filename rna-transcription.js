ef9f5ce1c1d89a7f80b12229eb8b13e383089171
Stefan Mehnert <stefanmehnert@gmail.com>
2015-11-15 19:15:50 +0100

    Translate a single Character from DNA to RNA, passing the first 4 Test's

--- rna-transcription.js
+++ rna-transcription.js
@@ -1,7 +1,16 @@
 var DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(input) {
-	
+	switch(input) {
+		case 'G':
+			return 'C';
+		case 'C':
+			return 'G';
+		case 'A':
+			return 'U';
+		case 'T':
+			return 'A';
+	}
 };
 
 DnaTranscriber.prototype.toDna = function(input) {
