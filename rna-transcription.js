ba187af1e1bc898a22c1ce2f6650cb774979e422
Stefan Mehnert <stefanmehnert@gmail.com>
2015-11-15 09:32:15 +0100

    Added an empty Object implementation

---
+++ rna-transcription.js
@@ -0,0 +1,11 @@
+var DnaTranscriber = function() {};
+
+DnaTranscriber.prototype.toRna = function(input) {
+	
+};
+
+DnaTranscriber.prototype.toDna = function(input) {
+	
+};
+
+module.exports = DnaTranscriber;
